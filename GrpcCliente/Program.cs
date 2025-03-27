3d3b52eb2ab3df7408c57ec4e1438001d1f1ff3b
inacio88 <inacio88@outlook.com>
2025-03-27 17:13:02 -0300

    main: customer

--- GrpcCliente/Program.cs
+++ GrpcCliente/Program.cs
@@ -7,16 +7,20 @@ Console.WriteLine("Inicio");
 
 
 
+// var channel = GrpcChannel.ForAddress("http://localhost:5227");
+// var client = new Greeter.GreeterClient(channel);
+
+// var helloRequest = new HelloRequest {Name = "Inácio"};
+// var reply = await client.SayHelloAsync(helloRequest);
+
+// System.Console.WriteLine(reply.Message);
+
+
 var channel = GrpcChannel.ForAddress("http://localhost:5227");
-var client = new Greeter.GreeterClient(channel);
+var CustomerClient = new Customer.CustomerClient(channel);
+var clientRequest = new CustomerLookupModel { UserId = 5};
+var customer = await CustomerClient.GetCustomerInfoAsync(clientRequest);
 
-var helloRequest = new HelloRequest {Name = "Inácio"};
-var reply = await client.SayHelloAsync(helloRequest);
-
-System.Console.WriteLine(reply.Message);
-
-
-
-
+System.Console.WriteLine(customer.FirstName + " " + customer.LastName);
 
 Console.WriteLine("Fim");
