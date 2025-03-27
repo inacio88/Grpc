7a2d445abe0aaac91c27bdf7ec64aff124a3252f
inacio88 <inacio88@outlook.com>
2025-03-27 16:02:48 -0300

    main: cliente

--- GrpcCliente/Program.cs
+++ GrpcCliente/Program.cs
@@ -1,15 +1,19 @@
-﻿Console.WriteLine("Inicio");
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+Console.WriteLine("Inicio");
 
 
 
 
 
+var channel = GrpcChannel.ForAddress("http://localhost:5227");
+var client = new Greeter.GreeterClient(channel);
 
+var helloRequest = new HelloRequest {Name = "Inácio"};
+var reply = await client.SayHelloAsync(helloRequest);
 
-
-
-
-
+System.Console.WriteLine(reply.Message);
 
 
 
